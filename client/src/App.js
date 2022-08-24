445bd286d1883e854097fa6e2cee6b124c85f853
German <german.rivarola11@gmail.com>
2022-08-24 18:59:31 -0300

    importe redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Col, Container, Row } from "react-bootstrap";
 import { Routes, Route } from 'react-router'
 import  Grid  from "./components/Grid";
@@ -6,11 +7,18 @@ import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import SignUp from "./components/SignUp";
 import Register from "./components/SignUp";
+=======
+import { Grid } from "./components/Grid";
+import { Route, Routes } from "react-router";
+import { ProductDetails } from "./commons/ProductDetails";
+
+>>>>>>> 37ca9ab (importe redux)
 
 
 
 function App() {
   return (
+<<<<<<< HEAD
     <div>
       <Navbar/>
       <Routes>
@@ -20,6 +28,16 @@ function App() {
         <Route path='/profile' element={<Profile />} />
       </Routes>
     </div>
+=======
+    <>
+     
+
+      <Routes>
+      <Route path="/" element={<Grid/>} />
+      <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </>
+>>>>>>> 37ca9ab (importe redux)
   );
 }
 
