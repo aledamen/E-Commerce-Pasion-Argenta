b2745f4f53d2707925f06df008cebe758116b13d
aledamen <alejandrodamen3@gmail.com>
2022-08-24 19:22:28 -0300

    delete redux

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,14 @@
 const product = require("./product")
+const auth = require ("./auth")
 const express = require('express')
 const router = express.Router()
 const user = require("./user")
 
 router.use ("/users", user)
 router.use ("/products", product)
+<<<<<<< HEAD
 
+=======
+router.use("/auth", auth)
+>>>>>>> fef8e4f (agregar ruta de login)
 module.exports = router;
