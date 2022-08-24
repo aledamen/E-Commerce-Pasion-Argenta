4dc3a2b9a6669241633a498bdc0714d1a1d91f81
CsCt00 <loza.leandro@gmail.com>
2022-08-24 20:24:48 -0300

    20:24hs

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 
-import { Grid } from "./components/Grid";
-import { Route, Routes } from "react-router";
-import { ProductDetails } from "./commons/ProductDetails";
 import { Col, Container, Row } from "react-bootstrap";
+import { Routes, Route } from 'react-router'
 import LogIn from "./components/LogIn";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import SignUp from "./components/SignUp";
 import Register from "./components/SignUp";
+import  Grid  from "./components/Grid";
+import { ProductDetails } from "./commons/ProductDetails";
+
 
 
 
@@ -16,17 +17,17 @@ function App() {
   return (
 
     <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Grid/>} />
+        <Navbar/>
+        <Routes>
         <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path='/' element={<Grid />} />
         <Route path='/signup' element={<SignUp/>} />
         <Route path='/login' element={<LogIn />} />
         <Route path='/profile' element={<Profile />} />
       </Routes>
     </div>
 
-  );
+   );
 }
 
 export default App;
