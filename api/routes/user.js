cef2dbdff31ab23980447ee4044ad59b21f19427
Fredyrooh <fredyrooh@gmail.com>
2022-08-24 18:54:53 -0300

    creando rutas

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,44 +1,43 @@
-const express = require("express")
-const Users = require("../models/Users")
-const router = express.Router()
+const express = require("express");
+const UserController = require("../controllers/user.controller");
+const Users = require("../models/Users");
+const router = express.Router();
 
 // registrar un usuario
-router.post("/register/", (req, res) => {
-    const createUser = new Users(req.body)
-    const user = createUser.save()
-    .then(user=> res.status(201).send(user))   
-  })
+router.post("/register/", UserController.createUser);
 
 // buscar todos los usuarios
-router.get("/all/",(req,res)=>{
-    Users.find({})
-    .sort({username:1 })
-    .then(users => res.send(users) )
-})
+router.get("/all/", UserController.getAllUsers);
 
 //buscar un usuario por id
-router.get("/:id",(req,res,next)=>{
-    const id = req.params.id
-    Users.findOne({ _id: id})
-    .then(user=>res.send(user))
-    .catch(err=>next)
-})
+router.get("/:id", UserController.getUser);
 
 //buscar favoritos favoritos
-router.get("/favorites/:id",(req,res,next)=>{
-    const id = req.params.id
-    Users.findOne({ _id: id})
-    .then(user=>res.send(user.favorites))
-    .catch(err=>next)
+router.get("/favorites/:id", (req, res, next) => {
+  const id = req.params.id;
+  Users.findOne({ _id: id })
+    .then((user) => res.send(user.favorites))
+    .catch((err) => next);
+});
+
+//ruta para modificar un usuario
+router.put("/modify/",(req,res)=>{
+    Users.updateOne({_id:req.body.id},{$set:req.body.mod})
+    .then(()=>res.sendStatus(204))
 })
-
+//ruta para promover a admin
+router.put("/toadmin/",(req,res)=>{
+    Users.updateOne({_id:req.body.id},{$set:{isAdmin: req.body.toggle}})
+    .then(()=>res.sendStatus(204))
+})
 //eliminar un usuario
-router.delete(("/:id",(req,res,next)=>{
-    const id = req.params.id
-    Users.deleteOne({ _id: id})
-    .then(() => res.sendStatus(204))
-    .catch(err => console.log(err))
-}))
+router.delete("/delete/:id", (req, res) => {
+    Users.deleteOne({ _id: req.params.id }).then((user) => {
+      res.status(204).send(user);
+    });
+  });
 
 
-module.exports = router
+module.exports = router;
+
+
