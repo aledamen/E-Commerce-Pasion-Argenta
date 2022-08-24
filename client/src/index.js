55a879e4628104c1fecf085a39f7add07f03281c
German <german.rivarola11@gmail.com>
2022-08-24 19:04:08 -0300

    importe el css

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import store from "./redux/store"
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+  <Router>  
+      <App /> 
+    </Router>
   </Provider>,
+  </React.StrictMode>
+  
 );
 
 
