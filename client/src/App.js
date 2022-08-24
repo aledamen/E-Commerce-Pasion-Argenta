dc8ed68585c94929edefc8371f1b2071cdfa89f8
aledamen <alejandrodamen3@gmail.com>
2022-08-24 18:55:33 -0300

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import { Grid } from "./components/Grid";
+import { Col, Container, Row } from "react-bootstrap";
+import { Routes, Route } from 'react-router'
+import  Grid  from "./components/Grid";
+import LogIn from "./components/LogIn";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import SignUp from "./components/SignUp";
+import Register from "./components/SignUp";
+
+
 
 function App() {
   return (
-    <>
-      <Grid />
-    </>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Grid />} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </div>
   );
 }
 
