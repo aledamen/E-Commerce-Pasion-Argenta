4a313850031f5b052642dd1e7b8117634cab08e6
German <german.rivarola11@gmail.com>
2022-08-25 18:49:15 -0300

    agregué el component home

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
-
-import { Col, Container, Row } from "react-bootstrap";
 import { Routes, Route } from 'react-router'
 import LogIn from "./components/LogIn";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import SignUp from "./components/SignUp";
-import Register from "./components/SignUp";
-import  Grid  from "./components/Grid";
 import { ProductDetails } from "./commons/ProductDetails";
-
-
+import { Main } from "./components/Home";
 
 
 
@@ -17,10 +12,10 @@ function App() {
   return (
 
     <div>
-        <Navbar/>
+        <Navbar/>        
         <Routes>
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path='/' element={<Grid />} />
+        <Route path='/' element={<Main />} />
         <Route path='/signup' element={<SignUp/>} />
         <Route path='/login' element={<LogIn />} />
         <Route path='/profile' element={<Profile />} />
