0a4c7c0dd355f63639b1e90d66215c48c2eba4a7
German <german.rivarola11@gmail.com>
2022-08-25 19:00:45 -0300

    le di formato al documento

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from "react-router";
 import LogIn from "./components/LogIn";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
@@ -6,23 +6,19 @@ import SignUp from "./components/SignUp";
 import { ProductDetails } from "./commons/ProductDetails";
 import { Main } from "./components/Home";
 
-
-
 function App() {
   return (
-
     <div>
-        <Navbar/>        
-        <Routes>
+      <Navbar />
+      <Routes>
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path='/' element={<Main />} />
-        <Route path='/signup' element={<SignUp/>} />
-        <Route path='/login' element={<LogIn />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
-
-   );
+  );
 }
 
 export default App;
