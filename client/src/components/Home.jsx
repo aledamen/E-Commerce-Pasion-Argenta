e1aaad238fa8dde9e8f8cca55ebfa1b2ae39ec80
German <german.rivarola11@gmail.com>
2022-08-25 18:59:27 -0300

    agregué la home como render principal

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousels from "./Carousels";
+import Grid from "./Grid";
+
+export const Main = () => {
+  return (
+    <>
+      <Carousels />
+      <br />
+      <Grid />
+    </>
+  );
+};
