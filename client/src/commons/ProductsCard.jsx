c94cfca00fc626f6c5a5f176594985703d5dc8f7
German <german.rivarola11@gmail.com>
2022-08-25 18:54:33 -0300

    agregué icono del carrito

--- client/src/commons/ProductsCard.jsx
+++ client/src/commons/ProductsCard.jsx
@@ -1,24 +1,47 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 export const ProductsCards = ({ props }) => {
-
-  
   return (
     <>
-    <Link to={`/products/${props._id}`}>
       <Card style={{ width: "18rem" }}>
-        <Card.Img
-          variant="top"
-          src={props.img}
-        />
+        <Link to={`/products/${props._id}`} style={{ textDecoration: "none" }}>
+          <Card.Img variant="top" src={props.img} />
+        </Link>
         <Card.Body>
-          <Card.Title>{props.name}</Card.Title>
-         
+          <Card.Title
+            style={{
+              width: "250px",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {props.name}
+          </Card.Title>
+
+          <div class="d-flex flex-row-reverse">
+            <h5 style={{ color: "black" }}>
+              {" "}
+              ${props.price}
+              <span>
+                <IconButton
+                  onClick={() => {
+                    alert("agregado al carrito");
+                  }}
+                  color="default"
+                  aria-label="add to shopping cart"
+                >
+                  <AddShoppingCartIcon fontSize="medium" />
+                </IconButton>
+              </span>
+            </h5>
+          </div>
         </Card.Body>
       </Card>
-      </Link>
     </>
   );
 };
