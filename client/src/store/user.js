660ee96874a0642d8e9ca71a7ee162be8ad349f8
aledamen <alejandrodamen3@gmail.com>
2022-08-29 13:06:51 -0300

    add new state

--- client/src/store/user.js
+++ client/src/store/user.js
@@ -9,28 +9,36 @@ import axios from "axios"
 // })
 export const sendMe = createAsyncThunk('ME', async () => {
     const auth = await axios.get('/api/auth/me')
-    // console.log("AUTH", auth)
+    const res = await axios.get(`/api/users/${auth.data._id}`)
+    return res.data 
+})
+
+export const signUpRequest = createAsyncThunk('SIGNUP', async (data) => {
+        const auth = await axios.post('/api/auth/signup', data)
+        const res = await axios.get(`/api/users/${auth.data._id}`)
+        return res.data 
+})
+
+export const LogInRequest = createAsyncThunk('LOGIN', async (data) => {
+    const auth = await axios.post('/api/auth/login', data)
     const res = await axios.get(`/api/users/${auth.data._id}`)
     return res.data
 })
 
-export const signUpRequest = createAsyncThunk('SIGNUP', async (data) => {
-    const res = await axios.post('/api/auth/signup', data)
-    return res.data
-})
-
-export const LogInRequest = createAsyncThunk('LOGIN', async (data) => {
-    const res = await axios.post('/api/auth/login', data)
-    return res.data
-})
 export const LogOutRequest = createAsyncThunk('LOGOUT', async () => {
     const res = await axios.post('/api/auth/logout')
     return res.data
-}) 
+})
+
+export const addToCart = createAsyncThunk('ADD_CART', async (data, thunkAPI) => {
+    const { user } = thunkAPI.getState()
+    const res = await axios.put(`/api/users/addtocart/${user._id}`, data)
+    return res.data // res entero
+})
 
 const userReducer = createReducer([], {
     // [setUser]: (state, action) => action.payload,
-    [sendMe.fulfilled]: (state, action) => action.payload,
+    [sendMe.fulfilled]: (state, action) => action.payload,//condicional
     [signUpRequest.fulfilled]: (state, action) => action.payload,
     [LogInRequest.fulfilled]: (state, action) => action.payload,
     [LogOutRequest.fulfilled]: (state, action) => action.payload,
