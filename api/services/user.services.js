7039464bed4ce1404b7f65def8bc8d31aca41188
CsCt00 <loza.leandro@gmail.com>
2022-08-29 19:07:45 -0300

    added email send on signup

--- api/services/user.services.js
+++ api/services/user.services.js
@@ -1,11 +1,14 @@
 const Products = require("../models/Products");
 const Users = require("../models/Users");
 const ObjectId = require("mongodb").ObjectId;
-//fix
+const sendEmail = require("../utils/mailer.utils")
+
 class UserService {
   static async createUser(body) {
     try {
       const user = new Users(body);
+      sendEmail(user, 0)
+      console.log("++++", user)
       return await user.save();
     } catch (error) {
       console.log(error);
@@ -49,7 +52,7 @@ class UserService {
   static async addToCart(id, { pid, amount }) {
     try {
       const user = await Users.find({_id: ObjectId(id),"cart._id": ObjectId(pid),});
-      const product = await Products.findById(pid, {name: 1, description: 1, img: 1,});
+      const product = await Products.findById(pid, {name: 1, description: 1, img: 1, price:1});
       
       if (user.length > 0) {
         return Users.updateOne(
@@ -65,6 +68,7 @@ class UserService {
                 _id: product._id,
                 name: product.name,
                 img: product.img,
+                price: product.price,
                 amount: amount,
               },
             },
@@ -75,6 +79,19 @@ class UserService {
       console.log(error);
     }
   }
+
+  static async removeFromCart(id, pid) {
+    try {
+      return await Users.updateOne(
+        { _id: ObjectId(id) },
+        { $pull: { cart: { _id: ObjectId(pid) } } }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   static async findInCart(id, pid) {
     try {
       return await Users.find({ _id: ObjectId(id), "cart._id": ObjectId(pid) });
