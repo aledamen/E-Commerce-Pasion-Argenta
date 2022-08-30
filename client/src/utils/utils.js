3884034f6bfa462ca4958d5bed84305d905292b0
Fredyrooh <fredyrooh@gmail.com>
2022-08-30 18:25:59 -0300

    creamos utils para mapear pages

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -4,3 +4,61 @@ export const subtotal = (cart) => {
     },0)
     return total
 }
+
+export const userOptions = [
+  {
+    title: "Mi carrito",
+    description: "Tenga acceso a su carrito de compras",
+    buttom: "cart",
+  },
+  {
+    title: "Mis favoritos",
+    description: "Tenga acceso a sus productos favoritos",
+    buttom: "favorites",
+  },
+  {
+    title: "Mis ordenes",
+    description: "Tenga acceso a sus ordenes creadas",
+    buttom: "Ordenes",
+  }
+];
+
+
+export const adminOptions = [
+    {
+      title: "Agregar Productos",
+      description: "Aqui puede agregar productos a su tienda",
+      buttom: "Agregar",
+      // name : "Nombre del Producto",
+      // description: "Descripcion del producto",
+      // img: "Imagen URL",
+      // price: "Precio del Producto",
+      // stock: "Stock agregado",
+      // category: "Categoria"
+    },
+    {
+      title: "Editar Productos",
+      description: "Aqui puede agregar productos a su tienda",
+      buttom: "Editar",
+    },
+    {
+      title: "Eliminar Productos",
+      description: "Aqui puede agregar productos a su tienda",
+      buttom: "Eliminar",
+    },
+    {
+      title: "Usuario a Admin",
+      description: "Aqui puede agregar productos a su tienda",
+      buttom: "Promover",
+    },
+    {
+      title: "Usuario a Admin",
+      description: "Aqui puede agregar productos a su tienda",
+      buttom: "Promover",
+    },
+    {
+      title: "Eliminar Usuario",
+      description: "Aqui puede agregar productos a su tienda",
+      buttom: "Eliminar",
+    },
+  ];
