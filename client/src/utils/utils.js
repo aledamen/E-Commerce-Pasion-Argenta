b3b6744aa89d1cb16b0a24289f941e5bfd93b960
lurenter <lucas.rentero22@gmail.com>
2022-08-30 18:22:26 -0300

    agregamos array para las vistas de las cards

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,6 +1,65 @@
 export const subtotal = (cart) => {
-    let total = cart?.reduce((acc,product) => {
-     return acc+=product.price*product.amount
-    },0)
-    return total
+  let total = cart?.reduce((acc,product) => {
+   return acc+=product.price*product.amount
+  },0)
+  return total
 }
+
+export const userOptions = [
+{
+  title: "Mi carrito",
+  description: "Tenga acceso a su carrito de compras",
+  buttom: "cart",
+},
+{
+  title: "Mis favoritos",
+  description: "Tenga acceso a sus productos favoritos",
+  buttom: "favorites",
+},
+{
+  title: "Mis ordenes",
+  description: "Tenga acceso a sus ordenes creadas",
+  buttom: "Ordenes",
+}
+];
+
+
+export const adminOptions = [
+  {
+    title: "Agregar Productos",
+    description: "Aqui puede agregar productos a su tienda",
+    buttom: "Agregar",
+    name : "Nombre del Producto",
+    // description: "Descripcion del producto",
+    // img: "Imagen URL",
+    // price: "Precio del Producto",
+    // stock: "Stock agregado",
+    // category: "Categoria"
+  },
+  {
+    title: "Editar Productos",
+    description: "Aqui puede agregar productos a su tienda",
+    buttom: "Editar",
+  },
+  {
+    title: "Eliminar Productos",
+    description: "Aqui puede agregar productos a su tienda",
+    buttom: "Eliminar",
+  },
+  {
+    title: "Usuario a Admin",
+    description: "Aqui puede agregar productos a su tienda",
+    buttom: "Promover",
+  },
+  {
+    title: "Usuario a Admin",
+    description: "Aqui puede agregar productos a su tienda",
+    buttom: "Promover",
+  },
+  {
+    title: "Eliminar Usuario",
+    description: "Aqui puede agregar productos a su tienda",
+    buttom: "Eliminar",
+  },
+];
+
