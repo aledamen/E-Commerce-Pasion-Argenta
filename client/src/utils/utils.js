70e6826e4d62cdc878b27ec10a38bec291667fa1
aledamen <alejandrodamen3@gmail.com>
2022-08-30 18:43:16 -0300

    add utils localstorage

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,7 +1,14 @@
 export const subtotal = (cart) => {
-    console.log("ESTE ES EL CART", cart)
     let total = cart?.reduce((acc,product) => {
      return acc+=product.price*product.amount
     },0)
     return total
+}
+
+export const saveToLocalStorage = (info) => {
+    let data = { ...info, amount: 1 }
+    let cart = []
+    cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(data)
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
