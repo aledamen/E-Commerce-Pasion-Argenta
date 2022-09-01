97ff24fc63e9777e3c8fe5e9ebb07b49ee43bbc6
CsCt00 <loza.leandro@gmail.com>
2022-09-01 11:05:53 -0300

    new order routes added

---
+++ api/routes/order.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const OrderController = require("../controllers/order.controller");
+const router = express.Router();
+const Orders = require("../models/Orders");
+
+//To get all orders, (Admin)
+router.get("/all", OrderController.findAllOrders);
+
+//To get specific order
+router.get("/orderid/:id", OrderController.findById);
+
+//To get specific user orders
+router.get("/userid/:id", OrderController.findByUserId);
+
+module.exports = router;
