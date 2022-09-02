7954201228a27e26e26685311aaadf124ad46f03
aledamen <alejandrodamen3@gmail.com>
2022-09-02 00:53:50 -0300

    delete dev comments

--- api/routes/order.js
+++ api/routes/order.js
@@ -2,13 +2,10 @@ const express = require('express')
 const OrderController = require('../controllers/order.controller')
 const router = express.Router()
 
-//To get all orders, (Admin)
 router.get('/all', OrderController.findAllOrders)
 
-//To get specific order
 router.get('/orderid/:id', OrderController.findById)
 
-//To get specific user orders
 router.get('/userid/:id', OrderController.findByUserId)
 
 module.exports = router
